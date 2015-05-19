b41092c5eed593e77b9da567eb646f9ef2a28255
Daniel Vinntreus <gudski@gmail.com>
2015-05-19 23:06:53 +0200

    Add simple login/logout

--- sidekick/urls.py
+++ sidekick/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     url(r'^candidate/', include('candidate.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', 'sidekick.views.login', name='login'),
+    url(r'^logout/', 'sidekick.views.logout', name='logout'),
+    url(r'^profile/', 'sidekick.views.profile', name='profile'),
 ]
--- sidekick/views.py
+++ sidekick/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib import auth, messages
+from django.http import HttpResponseRedirect
 from candidate.models import get_candidates
 
 
@@ -7,3 +9,33 @@ def home(request):
         'candidates': get_candidates()
     }
     return render(request, 'startpage.html', context)
+
+
+def login(request):
+    if request.method == 'GET':
+        context = {
+            'logged_in': request.user.is_authenticated
+        }
+        return render(request, 'login.html', context)
+    elif request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+        else:
+            messages.error(request, 'Wrong username or password')
+
+        return HttpResponseRedirect('/login/')
+    else:
+        raise Exception("Not supported method to login!")
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/')
+
+
+def profile(request):
+    return render(request, 'profile.html', {})
